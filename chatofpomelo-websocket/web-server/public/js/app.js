12b70ea204efce4063799ffb77fd72bb61e9c560
tomcat123456 <514658996@qq.com>
2015-01-19 15:48:52 +0800

    angular

---
+++ chatofpomelo-websocket/web-server/public/js/app.js
@@ -0,0 +1,20 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.run(function($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+});
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/index');
+    $stateProvider
+        .state('index', {
+            url: '/index',
+            views: {
+                '': {
+                    templateUrl: 'tpls/home.html'
+                }
+            }
+        })
+
+});
