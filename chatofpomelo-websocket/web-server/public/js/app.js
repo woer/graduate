09d295f265e6026ec6f41cfc7cc43b4b483559b6
tomcat123456 <514658996@qq.com>
2015-01-19 21:11:18 +0800

    完成静态页面
    完成静态页面

--- chatofpomelo-websocket/web-server/public/js/app.js
+++ chatofpomelo-websocket/web-server/public/js/app.js
@@ -13,9 +13,54 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/index',
             views: {
                 '': {
+                    templateUrl: 'tpls/html.html'
+                },
+                'topbar@index': {
+                    templateUrl: 'tpls/topbar.html'
+                },
+                'main@index': {
                     templateUrl: 'tpls/home.html'
                 }
             }
         })
-
+        .state('index.killgame', {
+            url: '/killgame',
+            views: {
+                'main@index': {
+                    templateUrl: 'tpls/killgame.html'
+                }
+            }
+        })
+        .state('index.introduce', {
+            url: '/introduce',
+            views: {
+                'main@index': {
+                    templateUrl: 'tpls/introduce.html'
+                }
+            }
+        })
+        .state('index.operate', {
+            url: '/operate',
+            views: {
+                'main@index': {
+                    templateUrl: 'tpls/operate.html'
+                }
+            }
+        })
+        .state('index.charts', {
+            url: '/charts',
+            views: {
+                'main@index': {
+                    templateUrl: 'tpls/charts.html'
+                }
+            }
+        })
+        .state('index.video', {
+            url: '/video',
+            views: {
+                'main@index': {
+                    templateUrl: 'tpls/video.html'
+                }
+            }
+        })
 });
